1223c4d276d39c3b99f12dc0857fb3b3611332a1
suralei <375262986@qq.com>
2019-05-23 22:36:53 +0800

    添加react测试

---
+++ src/react/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h2 data-testid="js-h2">京程一灯</h2>
+      <ul data-testid="js-ul">
+        <li>JavaScript</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  )
+}
