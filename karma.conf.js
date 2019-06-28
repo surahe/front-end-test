04a116189c79245efe708c727519eac318860c10
Sura <375262986@qq.com>
2019-06-28 20:54:27 +0800

    添加ChromeHeadless配置

--- karma.conf.js
+++ karma.conf.js
@@ -62,6 +62,12 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
 
     // Continuous Integration mode
