6fd89af9cb01955fb93aabc7532e7995f10ae9bb
Sura <375262986@qq.com>
2019-06-30 14:57:57 +0800

    修改部署脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,19 @@
 now_time=$(date +"%Y%m%d_%H%M%S")
 
+echo '停止 PM22 yd-test'
 pm2 stop yd-test
-tar -zcvf /data/bak/yd-test/$(date +"%Y%m%d_%H%M%S").tar.gz /www/jenkins/yd-test # 将当前web目录备份
-mv /data/jenkins/yd-test /www/jenkins/yd-test # 将jenkins传输的文件放到目录
 
+echo '将当前web目录备份'
+tar -zcvf /data/bak/yd-test/$(date +"%Y%m%d_%H%M%S").tar.gz /www/jenkins/yd-test
+
+echo '将jenkins传输的文件放到目录'
+mv /data/jenkins/yd-test /www/jenkins/yd-test
+
+echo '进入 /www/jenkins/yd-test'
 cd /www/jenkins/yd-test
 
+echo '安装依赖'
 npm install --registry=https://registry.npm.taobao.org --production
+
+echo '开启PM2'
 pm2 start
