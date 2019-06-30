eaa299bebb06d876ece8211ff933ac4ac696300f
Sura <375262986@qq.com>
2019-06-30 14:44:38 +0800

    修改部署脚本

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,10 @@
+now_time=$(date +"%Y%m%d_%H%M%S")
+
+pm2 stop yd-test
+tar -zcvf /data/bak/yd-test/$(date +"%Y%m%d_%H%M%S").tar.gz /www/jenkins/yd-test # 将当前web目录备份
+mv /data/jenkins/yd-test /www/jenkins/yd-test # 将jenkins传输的文件放到目录
+
+cd /www/jenkins/yd-test
+
 npm install --registry=https://registry.npm.taobao.org --production
-pm2 start app.js
+pm2 start
